20a58b7793b5f73cf8fa8d3051979e296581dfdf
abdelrahman hamdy <abdelrahmanhamdy320@gmail.com>
2025-11-02 23:01:34 +0200

    fix(deploy): force permissions on startup

--- docker/start.sh
+++ docker/start.sh
@@ -3,6 +3,10 @@
 # Exit immediately if a command exits with a non-zero status.
 set -e
 
+# Force correct permissions for storage and cache
+chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
+chmod -R 775 /var/www/html/storage /var/www/html/bootstrap/cache
+
 # Cache configuration
 php artisan config:cache
 php artisan route:cache
