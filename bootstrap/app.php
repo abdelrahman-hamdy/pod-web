4d9de3ca52d6c9b7c97a4c93c96a5fbf9fa3ff33
abdelrahman hamdy <abdelrahmanhamdy320@gmail.com>
2025-11-03 02:41:15 +0200

    fix: correct logo path and add placeholder

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,6 +5,10 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
+    ->withProviders([
+        // App\Providers\BroadcastServiceProvider::class,
+        \App\Providers\Filament\AdminPanelProvider::class,
+    ], true)
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         api: __DIR__.'/../routes/api.php',
@@ -37,5 +41,13 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        if (! app()->environment('local')) {
+            $exceptions->render(function (\Symfony\Component\HttpKernel\Exception\NotFoundHttpException $e, $request) {
+                return response()->view('errors.404', [], 404);
+            });
+
+            $exceptions->render(function (\Throwable $e, $request) {
+                return response()->view('errors.500', [], 500);
+            });
+        }
     })->create();
